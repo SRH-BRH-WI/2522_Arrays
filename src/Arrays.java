69c7fe4a5e8d6f947b286950d5c89cae8bad7402
Dr. Dillinger <peter.dillinger@srh.de>
2023-02-16 15:57:22 +0100

    Ein paar kleine Fingerübungen mit Arrays. 1. Aufgabe: kleinsterWert im Array finden!

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,5 +1,34 @@
 public class Arrays {
-    public static void main(String[] args) {
 
+    public static boolean sucheImArray(int[] array, int datum) {
+        for (int i=0; i < array.length; i++) {
+            if (datum == array[i])
+                return true;
+        }
+        return false;
+    }
+
+    public static int kleinsterWertImArray(int[] array) {
+        // TODO
+    }
+
+
+    public static void main(String[] args) {
+//        // Anlegen eines Arrays
+//        int[] array = { 5, 4, 3, 2, 1 };
+//        //              0  1  2  3  4
+//        // festes Array mit 5 Elementen und ihren entsprechenden Werten
+//        int[] nochEinArray = new int[ 1000 ];
+//        int[] einWeiteresArray = array;
+
+//        einWeiteresArray[3] = -1;
+//        System.out.println( array[3] );
+//        System.out.println( nochEinArray[473] );
+//        System.out.println( einWeiteresArray[3] );
+
+        int[] datenreihe = { 17, 3, 29, 22, 5, 11, 9, 13, 7, 12 };
+
+        System.out.println( "Ist die 13 im Array? " + sucheImArray(datenreihe, 13) );
+        System.out.println( "Ist die 42 im Array? " + sucheImArray(datenreihe, 42) );
     }
 }
